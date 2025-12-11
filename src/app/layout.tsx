c06eab4c658c87738572a9a7cdfb16acbcf7a863
wcyu0527-cmyk <wcyu0527@gmail.com>
2025-12-11 16:28:58 +0800

    fix: update css variables syntax and revert default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider defaultTheme="dark" storageKey="app-theme">
+        <ThemeProvider defaultTheme="system" storageKey="app-theme">
           {children}
         </ThemeProvider>
       </body>
